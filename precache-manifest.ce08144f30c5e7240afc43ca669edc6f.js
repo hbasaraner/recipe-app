5733f018ccc51a85bdcd5e455e3d6892d6e7ee35
Hayrettin Basaraner <hbasaraner@gmail.com>
2020-10-22 14:35:13 +0300

    Updates

---
+++ precache-manifest.ce08144f30c5e7240afc43ca669edc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d002290735fbacab8999d261c02f0f75",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "be4f919f7ece70a5cdb7",
+    "url": "/recipe-app/static/css/main.5689e08a.chunk.css"
+  },
+  {
+    "revision": "0ac20a67948723e4bcf3",
+    "url": "/recipe-app/static/js/2.d0140bc9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/recipe-app/static/js/2.d0140bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4f919f7ece70a5cdb7",
+    "url": "/recipe-app/static/js/main.02171965.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
